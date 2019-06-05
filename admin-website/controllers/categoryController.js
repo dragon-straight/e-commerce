0102e4aedc43899d66ddac978f963f7359981606
Luu Tuan Nguyen <miketuannguyen@gmail.com>
2019-06-05 21:12:37 +0700

    add file docx + nguyên's work

--- admin-website/controllers/categoryController.js
+++ admin-website/controllers/categoryController.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 const mongoDB = 'mongodb+srv://dragon-straight:8910JQKA@cluster0-dqpzz.mongodb.net/e-commerce';
-
 const Category = require('../models/category');
 const productDao = require('../models/dao/productDao');
 
 exports.category_stall=function(req,res)
 {
     res.render('category/stall', { pageTitle: 'Quản lý gian hàng' });
-}
+};
 
 exports.category_list= async function(req,res)
 {
