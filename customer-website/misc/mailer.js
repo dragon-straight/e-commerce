bfaee16e50a09616a7acd2f2519e6d7d9346a162
NGUYỄN VĂN PHƯƠNG NGHI <1612415@student.hcmus.edu.vn>
2019-06-15 17:35:31 +0700

    Update mailer.js

--- customer-website/misc/mailer.js
+++ customer-website/misc/mailer.js
@@ -1,23 +1,24 @@
 const nodemailer=require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport');
 
 const config =require('../config/mailer')
 
 
-const transporter=nodemailer.createTransport(
-    {
-        service:'gmail',
-        auth:{
-            user:config.USER,
-            pass:config.PASS
-        }
+const transporter=nodemailer.createTransport(smtpTransport({
+    service:'gmail',
+    auth:{
+        user:config.USER,
+        pass:config.PASS
     }
+})
+    
 );
 
 
 
 const sendMail = (email, subject, text, cb) => {
     const mailOptions = {
-        from: 'Verify', // TODO replace this with your own email
+        from: 'nghi.clone001@gmail.com', // TODO replace this with your own email
         to: email, // TODO: the receiver email has to be authorized for the free tier
         subject,
         text
